a7892dcee40062b7900fe245596b2bf93686ca5e
Albert K Petros <albertkpetrose@gmail.com>
2020-06-23 21:12:26 +0530

    added login,register,dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BuyerDashboardComponent } from './buyer-dashboard/buyer-dashboard.component';
+import { SellerDashboardComponent } from './seller-dashboard/seller-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BuyerDashboardComponent,
+    SellerDashboardComponent
   ],
   imports: [
     BrowserModule,
