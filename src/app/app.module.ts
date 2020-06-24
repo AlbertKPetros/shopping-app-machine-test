f7f3ad2c3bb52e4c58a30e5f92310198b3f54340
Albert K Petros <albertkpetrose@gmail.com>
2020-06-24 08:00:01 +0530

    added basic template for login and register with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { BuyerDashboardComponent } from './buyer-dashboard/buyer-dashboard.component';
-import { SellerDashboardComponent } from './seller-dashboard/seller-dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    BuyerDashboardComponent,
-    SellerDashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
